30978e00fecdd012e037afcecd99cb825ebaeb3e
valib <liborvala@seznam.cz>
2020-05-23 01:18:00 +0200

    Update

--- src/main/java/net/pms/util/ImdbUtil.java
+++ src/main/java/net/pms/util/ImdbUtil.java
@@ -100,7 +100,8 @@ public class ImdbUtil {
 							continue;
 						}
 						double score = new JaroWinklerDistance().apply(nfoFileName, entryName);
-						if (score <= 0.85) { // threshold to add candidate to map. XXX it should be adjusted
+						if (score <= 0.85) { // threshold to add candidate to map. XXX it should be adjusted.
+											// The value 0.5 should be enough to insert relevant candidate.
 							candidates.put(entry, Double.valueOf(score));
 						}
 					}
